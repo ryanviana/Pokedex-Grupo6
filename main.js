86f6310c0f4e5f86204d6ed017dd3f91bb1e11f4
trizcard <beatrizco@usp.br>
2021-08-09 14:36:02 -0300

    adding main

---
+++ main.js
@@ -0,0 +1,35 @@
+const pokeContainer = document.getElementById ('poke_container');
+const pokeNumber = 150;
+
+const getPoke = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    pokeCard (pokemon);
+}
+
+const pokeCard = (pokemon) => {
+    const pokeElement = document.createElement('div');
+    pokeElement.classList.add ('pokemon');
+    const {id, name, sprites, types} = pokemon;
+    const type = types[0].type.name;
+    const pokeInnerHTML = `
+        <div class="img-container">
+            <img src="${sprites.front_default}" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">${name}</span>
+            <h3 class="name">${id}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>`
+    pokeElement.innerHTML = pokeInnerHTML;
+    pokeContainer.appendChild (pokeElement);
+}
+
+const fetchPoke = async => {
+    for (let i = 1; i <= 150; i++){
+        await getPoke (i);
+    }
+}
+
+fetchPoke();
